40fe504136ea13a2539171e6ff1a5807b8cbf6a0
Adolfo Ernesto Cortez Barrera <116194976+AdolDeveloper@users.noreply.github.com>
2023-12-21 21:44:56 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+setTimeout(function() {
+    const message = document.getElementById('result');
+    message.innerHTML = "Abrazo enviado!"
+
+}, 6200);
+setTimeout(function() {
+
+    const message2 = document.getElementById('result2');
+    message2.innerHTML = "La Amo Muchote Mi Niña <3"
+}, 6800);
+setTimeout(function() {
+    const message = document.getElementById('state-message');
+    message.innerHTML = "&nbsp;"
+ 
+}, 6000);
+setTimeout(function() {
+ 
+    const message2 = document.getElementById('state-message');
+    message2.innerHTML = "&nbsp;"
+}, 6000);
